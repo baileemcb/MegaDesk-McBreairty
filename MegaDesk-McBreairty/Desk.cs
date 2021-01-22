8d3e7a17e08246c1e4540bec594de9588e2e8f82
Bailee <rob16020@byui.edu>
2021-01-22 07:15:06 -0700

    Week 1 End

--- MegaDesk-McBreairty/Desk.cs
+++ MegaDesk-McBreairty/Desk.cs
@@ -17,6 +17,14 @@ namespace MegaDesk_McBreairty
     }
     public class Desk
     {
+        //constants
+        public const short MIN_WIDTH = 24;
+        public const short MAX_WIDTH = 96;
+        public const short MIN_DEPTH = 12;
+        public const short MAX_DEPTH = 48;
+        public const short MIN_DESK_DRAWERS = 0;
+        public const short MAX_DESK_DRAWERS = 7; 
+
         // prop tab tab to do that thing
         public decimal Width { get; set; }
 
